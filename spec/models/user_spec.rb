fd71ec75591988f2a50db8cbc0d70b0cc9fb022d
Lindsey Saari <lisaari@nmu.edu>
2014-05-30 15:46:09 -0400

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,21 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
+  
+  it { should respond_to(:authenticate) }
+    it { should respond_to(:admin) }
+
+    it { should be_valid }
+    it { should_not be_admin }
+
+    describe "with admin attribute set to 'true'" do
+      before do
+        @user.save!
+        @user.toggle!(:admin)
+      end
+
+      it { should be_admin }
+  end
 
   subject { @user }
 
@@ -97,5 +112,10 @@ describe User do
         it { should_not eq user_for_invalid_password }
         specify { expect(user_for_invalid_password).to be_false }
       end
+    end
+    
+    describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
     end
 end
